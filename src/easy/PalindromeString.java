1045cabfc746a5fc3e8feff915aa2a75b438a5bb
tahahanci <taha.hanci@std.bogazici.edu.tr>
2025-08-29 20:04:47 +0300

    add PalindromeString and TwoSumII classes for solving string and array problems

---
+++ src/easy/PalindromeString.java
@@ -0,0 +1,20 @@
+package easy;
+
+public class PalindromeString {
+
+    public boolean checkIfPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
---
+++ src/medium/TwoSumII.java
@@ -0,0 +1,34 @@
+package medium;
+
+import java.util.Arrays;
+
+public class TwoSumII {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int current = numbers[left] + numbers[right];
+
+            if (current == target) {
+                return new int[] {left + 1, right + 1};
+            }
+
+            if (current > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        return new int[] {-1, -1};
+    }
+
+    public static void main(String[] args) {
+        TwoSumII twoSumII = new TwoSumII();
+        int[] numbers = {2,7,11,15};
+
+        System.out.println(Arrays.toString(twoSumII.twoSum(numbers, 9)));
+    }
+}
