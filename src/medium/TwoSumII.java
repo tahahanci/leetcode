ecdb9d98db1fc2647704e6c08fd57692eff221f7
tahahanci <taha.hanci@std.bogazici.edu.tr>
2025-08-29 20:05:17 +0300

    add comment for Leetcode 167 in TwoSumII class

--- src/medium/TwoSumII.java
+++ src/medium/TwoSumII.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class TwoSumII {
 
+    // Leetcode 167.
+
     public int[] twoSum(int[] numbers, int target) {
         int left = 0;
         int right = numbers.length - 1;
