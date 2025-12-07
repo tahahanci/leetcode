6c13713299985f8c72c4aaeab4a1dc83520a9d3f
tahahanci <taha.hanci@std.bogazici.edu.tr>
2025-12-07 23:19:19 +0300

    add ReversePrefixOfWord class to reverse the prefix of a word up to a specified character

---
+++ src/com/hanci/easy/ReversePrefixOfWord.java
@@ -0,0 +1,24 @@
+package com.hanci.easy;
+
+import java.util.Arrays;
+
+public class ReversePrefixOfWord {
+
+    public String reversePrefix(String word, char ch) {
+        int left = 0, right = word.indexOf(ch);
+        char[] characters = word.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (left < right) {
+            char temp = characters[left];
+            characters[left++] = characters[right];
+            characters[right--] = temp;
+        }
+
+        for (char c : characters) {
+            stringBuilder.append(c);
+        }
+
+        return stringBuilder.toString();
+    }
+}
